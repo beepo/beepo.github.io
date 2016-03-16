6577ffc0e6a9388bb88d227c10f657a6673f8707
Dmitry Ulupov <dima@ulupov.com>
2016-03-16 22:37:39 +0800

    added stars counting

--- game.jsx
+++ game.jsx
@@ -2,11 +2,13 @@ const UPDATE_LENGTH = 'UPDATE_LENGTH';
 const START_GAME = 'START_GAME';
 const STOP_GAME = 'STOP_GAME';
 const PRESSED_THE_BUTTON = 'PRESSED_THE_BUTTON';
+const BEEPED = 'BEEPED';
 const SOUND_500hz_2s = '/audiocheck.net_sin_500Hz_-3dBFS_2s.wav';
 const SOUND_1000hz_01s = '/audiocheck.net_sin_1000Hz_-3dBFS_0.1s.wav';
 
 var startGame = () => {return {type: START_GAME}};
 var stopGame = () => {return {type: STOP_GAME}};
+var beeped = () => {return {type: BEEPED}};
 var pressedTheButton = () => {return {type: PRESSED_THE_BUTTON, date: new Date()}};
 
 function updateLength(delay) {
@@ -19,13 +21,15 @@ function updateLength(delay) {
 const initialState = {
     delay: 1000,
     running: false,
-    lastButtonPressed: null
+    stars: [],
+    maxStars: 5
 };
 
 function beepoApp(state, action) {
     if (typeof state === 'undefined') {
         return initialState
     }
+    var newStars;
 
     switch (action.type) {
         case UPDATE_LENGTH:
@@ -37,15 +41,31 @@ function beepoApp(state, action) {
             return state;
         case START_GAME:
             return Object.assign({}, state, {
-                running: true
+                running: true,
+                stars: []
             });
         case STOP_GAME:
             return Object.assign({}, state, {
                 running: false
             });
         case PRESSED_THE_BUTTON:
+            newStars = state.stars.map((s) => {return s});
+            console.log(newStars);
+            if (newStars.length) {
+                newStars.pop();
+                newStars.push(true);
+            }
             return Object.assign({}, state, {
-                lastButtonPressed: action.date
+                stars: newStars
+            });
+        case BEEPED:
+            console.log(state.stars);
+            newStars = state.stars.map((s) => {return s});
+            console.log(newStars);
+            newStars.push(false);
+            console.log(newStars);
+            return Object.assign({}, state, {
+                stars: newStars
             });
         default:
             return state;
@@ -69,9 +89,14 @@ var audio_1000hz_01s = new Audio(SOUND_1000hz_01s);
 var beep = () => {
     var state = store.getState();
     if (state.running) {
-        setTimeout(() => {
-            audio_1000hz_01s.play()
-        }, 13);
+        if (state.stars.length < state.maxStars) {
+            setTimeout(() => {
+                audio_1000hz_01s.play()
+            }, 13);
+            store.dispatch(beeped());
+        } else {
+            store.dispatch(stopGame());
+        }
     }
     setTimeout(beep, state.delay > 100 ? state.delay : 100);
 };
@@ -87,7 +112,13 @@ var GameBox = React.createClass({
     render: function () {
         return (
             <div className="row">
-                <div className="col-xs-8"></div>
+                <div className="col-xs-8">
+                    <div className="row">
+                        <div className="col-xs-1"><ConnectedSmile/></div>
+                        <div className="col-xs-11">
+                        </div>
+                    </div>
+                </div>
                 <div className="col-xs-4">
                     <ControlForm/>
                 </div>
@@ -95,6 +126,18 @@ var GameBox = React.createClass({
         );
     }
 });
+
+const Smile = ({show}) => {
+    if (show) {
+        return (<div>:-)</div>);
+    } else if (show === false) {
+        return (<div>:-(</div>);
+    } else {
+        return (<div>x-|</div>);
+    }
+};
+
+const ConnectedSmile = ReactRedux.connect((state) => {return {show: state.stars.length ? state.stars[state.stars.length - 1] : undefined }})(Smile);
 
 var ControlForm = React.createClass({
     render: function () {
